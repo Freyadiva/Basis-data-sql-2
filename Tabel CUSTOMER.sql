b4fb541cbddc2b9fff11ead00c9c12a3fc0f5ba6
Freya <hanikafreyadiva@gmail.com>
2024-09-19 10:00:54 +0700

    Add files via upload

---
+++ Tabel CUSTOMER.sql
@@ -0,0 +1,10 @@
+CREATE TABLE CUSTOMER
+(
+CustomerID INT PRIMARY KEY, 
+CustomerName VARCHAR(100), 
+ContactName VARCHAR(100), 
+Address VARCHAR(255), 
+City VARCHAR(100), 
+PostalCode VARCHAR(20), 
+Country VARCHAR(100) 
+);
